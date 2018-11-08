79396591600345b15aba10d2d869e0360064e721
qed0711 <qdizon@gmail.com>
2018-11-08 17:43:55 -0500

    contact form working with form spree

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,21 +1,39 @@
 import React from 'react'
 
+// import $ from 'jquery';
+
 const EmailForm = () => {
 
+
+    // formspree no longer works with ajax for free. use below code with paied account
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     console.log(e.target.getAttribute("action"))
+    //     const data = {
+    //         replyTo: e.target.children[2].value,
+    //         subject: e.target.children[6].value,
+    //         messageBody: e.target.children[10].value.value
+    //     }
+    //     $.post(e.target.getAttribute("action"), data).done(function(error){
+    //         console.log(error)
+    //     })
+    // }
+
     return(
-        <form>
-            <label>Your Email Address:</label>
-            <br />
-            <input />
-            <br />
-            <label>Message Title:</label>
-            <br />
-            <input />
-            <br />
-            <label>Your Message:</label>
-            <br />
-            <textarea></textarea>
-            <br />
+        <form action="https://formspree.io/qdizon@gmail.com" method="POST">
+            
+            <label for="name">Your Name:</label><br />
+            <input type="text" id="name" name="name" required/><br />
+            
+            <label for="reply-to">Your Email Address:</label><br />
+            <input type="email" id="reply-to" name="reply-to" required/><br />
+
+            <label for="subject">Subject:</label><br />
+            <input type="text" id="subject" name="subject"/><br />
+
+            <label for="message-body">Your Message:</label><br />
+            <textarea id="message-body" name="message-body"></textarea><br />
+
             <input type="submit" value="Send Message"/>
         </form>
     )
