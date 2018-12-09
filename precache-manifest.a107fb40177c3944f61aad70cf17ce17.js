673d8dcb09303bec5a31312e4ec8a0f35ac17c05
QED0711 <qdizon@gmail.com>
2018-12-09 14:44:08 -0500

    Updates

---
+++ precache-manifest.a107fb40177c3944f61aad70cf17ce17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b96579f4c306c63c1563133d67d6838b",
+    "url": "/quinndizon-react/static/media/headshot.b96579f4.jpg"
+  },
+  {
+    "revision": "8629315f1dbffaa7007f66215ee78cde",
+    "url": "/quinndizon-react/static/media/MainImage.8629315f.jpg"
+  },
+  {
+    "revision": "90cd797dd250e90d8026",
+    "url": "/quinndizon-react/static/js/runtime~main.90cd797d.js"
+  },
+  {
+    "revision": "a634832ba424758a23cb",
+    "url": "/quinndizon-react/static/js/main.a634832b.chunk.js"
+  },
+  {
+    "revision": "b5975168f2c72556df24",
+    "url": "/quinndizon-react/static/js/1.b5975168.chunk.js"
+  },
+  {
+    "revision": "a634832ba424758a23cb",
+    "url": "/quinndizon-react/static/css/main.66536621.chunk.css"
+  },
+  {
+    "revision": "26a320615185ab1b8b8a1016738fee8c",
+    "url": "/quinndizon-react/index.html"
+  }
+];
