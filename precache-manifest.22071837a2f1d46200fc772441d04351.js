019a2a0a3a900d81e098a6cc235894d17d6b4dff
QED0711 <qdizon@gmail.com>
2019-03-02 12:16:01 -0500

    Updates

---
+++ precache-manifest.22071837a2f1d46200fc772441d04351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "430a7a800f19d3a5fcf9",
+    "url": "/static/css/main.6b4b4701.chunk.css"
+  },
+  {
+    "revision": "430a7a800f19d3a5fcf9",
+    "url": "/static/js/main.430a7a80.chunk.js"
+  },
+  {
+    "revision": "42240bd89cab2a02b9e0",
+    "url": "/static/js/1.42240bd8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8629315f1dbffaa7007f66215ee78cde",
+    "url": "/static/media/MainImage.8629315f.jpg"
+  },
+  {
+    "revision": "b96579f4c306c63c1563133d67d6838b",
+    "url": "/static/media/headshot.b96579f4.jpg"
+  },
+  {
+    "revision": "60ec44d31a06a028905479f2334fa3c7",
+    "url": "/index.html"
+  }
+];
